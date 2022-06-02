94db08e91941d79dadc5abdc03cefd5c60de6eb7
Olha Shyshova <oshys@softserveinc.com>
2022-06-02 19:34:34 +0200

    fix

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HobbiesComponent implements OnInit {
 
+  hobbies = [
+    { name: 'Snowboard', image: 'https://images.unsplash.com/photo-1614358536373-1ce27819009e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'},
+    { name: 'Travel', image: 'https://images.unsplash.com/photo-1488085061387-422e29b40080?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
